13f59cebd1d6ae0b40c6453ca611a7916d814b00
Jxstxs <juleb0202@gmail.com>
2022-09-05 00:06:18 +0200

    feat: template configurations

---
+++ lua/nvimconf/modules/keystack.lua
@@ -0,0 +1,4 @@
+
+return function (keystack)
+    keystack.setup({})
+end
---
+++ lua/nvimconf/modules/worktree.lua
@@ -0,0 +1,15 @@
+return function(worktree)
+	worktree.setup({
+		-- change_directory_command = <str> -- default: "cd",
+		-- update_on_change = <boolean> -- default: true,
+		-- update_on_change_command = <str> -- default: "e .",
+		-- clearjumps_on_change = <boolean> -- default: true,
+		-- autopush = <boolean> -- default: false,
+	})
+
+	worktree.on_tree_change(function(op, metadata)
+		if op == worktree.Operations.Switch then
+			print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+		end
+	end)
+end
