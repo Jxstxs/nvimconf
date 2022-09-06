b431d1a9f83f28cee52e44d6598ad6e547ed3359
Jxstxs <juleb0202@gmail.com>
2022-09-06 21:14:58 +0200

    feat: added some imports

--- lua/nvimconf/core/init.lua
+++ lua/nvimconf/core/init.lua
@@ -24,21 +24,29 @@ import("bufferline", require("nvimconf.modules.bufferline"))
 import("indent_blankline", require("nvimconf.modules.indent"))
 import("hlargs", require("nvimconf.modules.hlargs"))
 import("dressing", require("nvimconf.modules.dressing"))
+import("telescope", require("nvimconf.modules.telescope"))
 
 -- Rnvimr
 import("nvimconf.modules.rnvimr")
 
 -- Key Stuff
--- import("keystack", require("nvimconf.modules.keystack"))
-import("legendary", require("nvimconf.modules.legendary"))
+import("keystack", require("nvimconf.modules.keystack"))
 import("which-key", require("nvimconf.modules.whichkey"))
+import("legendary", require("nvimconf.modules.legendary"))
 
 -- Git Stuff
+import("gitsigns", require("nvimconf.modules.gitsigns"))
 import("nvimconf.modules.lazygit")
 import("git-worktree", require("nvimconf.modules.worktree"))
 
+-- Writing Stuff
+-- import("nvim-treesitter", require("nvimconf.modules.treesitter"))
+import("nvim-autopairs", require("nvimconf.modules.autopairs"))
+import("Comment", require("nvimconf.modules.comment"))
+
 -- Plugins
 import("nvimconf.modules.plugins")
+
 -- Lsp Stuff
 -- import("mason", require("nvimconf.modules.lsp").manson)
 -- import("manson-lspconfig", require("nvimconf.modules.lsp").manson_lspconfig)
