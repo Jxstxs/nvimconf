c339ea5a2e6a14f34bb5e94004b7376c7e2c4fb2
Jxstxs <juleb0202@gmail.com>
2022-09-07 23:39:18 +0200

    fmt: stylua

--- lua/nvimconf/core/keymap.lua
+++ lua/nvimconf/core/keymap.lua
@@ -8,39 +8,39 @@ vim.keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 
 nmap({
-    -- Easier Window Movement
-    {"<C-h>", "<C-w>h", opts},
-    {"<C-j>", "<C-w>j", opts},
-    {"<C-k>", "<C-w>k", opts},
-    {"<C-l>", "<C-w>l", opts},
+	-- Easier Window Movement
+	{ "<C-h>", "<C-w>h", opts },
+	{ "<C-j>", "<C-w>j", opts },
+	{ "<C-k>", "<C-w>k", opts },
+	{ "<C-l>", "<C-w>l", opts },
 
-    -- Resizing Windows
-    -- {"<C-H>", cmd("resize -2"), opts},
-    -- {"<C-J>", cmd("resize +2"), opts},
-    -- {"<C-K>", cmd("vertical resize -2"), opts},
-    -- {"<C-L>", cmd("vertical resize +2"), opts},
+	-- Resizing Windows
+	-- {"<C-H>", cmd("resize -2"), opts},
+	-- {"<C-J>", cmd("resize +2"), opts},
+	-- {"<C-K>", cmd("vertical resize -2"), opts},
+	-- {"<C-L>", cmd("vertical resize +2"), opts},
 
-    -- Buffer Navigation
-    -- {"<S-h>", cmd("bnext"), opts},
-    -- {"<S-l>", cmd("bprevious"), opts},
+	-- Buffer Navigation
+	-- {"<S-h>", cmd("bnext"), opts},
+	-- {"<S-l>", cmd("bprevious"), opts},
 })
 
 vmap({
-    -- Better paste
-    {"p", '"_dP', opts},
+	-- Better paste
+	{ "p", '"_dP', opts },
 
-    -- Better Indentations
-    {"<", "<gv", opts},
-    {">", ">gv", opts},
+	-- Better Indentations
+	{ "<", "<gv", opts },
+	{ ">", ">gv", opts },
 })
 
 imap({
-    -- Quit Insert Mode
-    {"jj", "<ESC>", opts},
+	-- Quit Insert Mode
+	{ "jj", "<ESC>", opts },
 
-    -- Easier Movement
-    {"<C-h>", "<Left>", opts},
-    {"<C-j>", "<Down>", opts},
-    {"<C-k>", "<Up>", opts},
-    {"<C-l>", "<Right>", opts},
+	-- Easier Movement
+	{ "<C-h>", "<Left>", opts },
+	{ "<C-j>", "<Down>", opts },
+	{ "<C-k>", "<Up>", opts },
+	{ "<C-l>", "<Right>", opts },
 })
