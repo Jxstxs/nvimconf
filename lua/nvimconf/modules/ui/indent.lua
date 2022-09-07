a7711e2851bda92f9f575ca24b03667b68d0c640
Jxstxs <juleb0202@gmail.com>
2022-09-08 01:48:28 +0200

    fix: removed space coneal for better performance

--- lua/nvimconf/modules/ui/indent.lua
+++ lua/nvimconf/modules/ui/indent.lua
@@ -1,7 +1,7 @@
 return function(indent)
 	vim.opt.list = true
 	vim.opt.listchars:append("eol:↴")
-	vim.opt.listchars:append("space:⋅")
+	-- vim.opt.listchars:append("space:⋅")
 
 	indent.setup({
 		show_end_of_line = true,
