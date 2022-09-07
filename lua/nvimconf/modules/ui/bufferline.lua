e509a3b5ac87947fddb84240f14f27d260c36b2f
Jxstxs <juleb0202@gmail.com>
2022-09-08 01:48:09 +0200

    fix: better ui now

--- lua/nvimconf/modules/ui/bufferline.lua
+++ lua/nvimconf/modules/ui/bufferline.lua
@@ -72,7 +72,7 @@ return function(bufferline)
 			persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
 			-- can also be a table containing 2 custom separators
 			-- [focused and unfocused]. eg: { '|', '|' }
-			separator_style = "slant", --  | "thick" | "thin" | { 'any', 'any' },
+			separator_style = "thin", --  | "thick" | "thin" | { 'any', 'any' },
 			enforce_regular_tabs = false, --  | true,
 			always_show_bufferline = true, --  | false,
 			sort_by = "insert_after_current", --  |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
