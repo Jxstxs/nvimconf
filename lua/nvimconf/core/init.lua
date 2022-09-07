11d38b0f970d8f0726292f0ab29112c2ce852ba9
Jxstxs <juleb0202@gmail.com>
2022-09-07 23:39:01 +0200

    fix: rearranged imports and added new config structure

--- lua/nvimconf/core/init.lua
+++ lua/nvimconf/core/init.lua
@@ -16,46 +16,51 @@ _import.config(require("nvimconf.modules.import"))
 local import = _import.import
 
 -- Impatient Loading for fast times
-
+import("impatient", require("nvimconf.modules.impatient"))
 
 -- Theme
 import("catppuccin", require("nvimconf.modules.colorscheme"))
-vim.cmd [[colorscheme catppuccin]]
+--
+-- Autocommands
+import("nvimconf.modules.autocommands")
 
--- Ui Stuff
-import("bufferline", require("nvimconf.modules.bufferline"))
-import("indent_blankline", require("nvimconf.modules.indent"))
-import("hlargs", require("nvimconf.modules.hlargs"))
-import("dressing", require("nvimconf.modules.dressing"))
-import("telescope", require("nvimconf.modules.telescope"))
+-- Filemanager
+import("nvim-tree", require("nvimconf.modules.filemanager.nvim-tree"))
+import("nvimconf.modules.filemanager.rnvimr")
 
--- Rnvimr
-import("nvimconf.modules.rnvimr")
+-- Git
+import("gitsigns", require("nvimconf.modules.git.gitsigns"))
+import("git-worktree", require("nvimconf.modules.git.worktree"))
+import("nvimconf.modules.git.lazygit")
 
--- Key Stuff
-import("keystack", require("nvimconf.modules.keystack"))
-import("which-key", require("nvimconf.modules.whichkey"))
-import("legendary", require("nvimconf.modules.legendary"))
+-- Keymap
+import("keystack", require("nvimconf.modules.keymap.keystack"))
+import("which-key", require("nvimconf.modules.keymap.whichkey"))
+import("legendary", require("nvimconf.modules.keymap.legendary"))
 
--- Git Stuff
-import("gitsigns", require("nvimconf.modules.gitsigns"))
-import("nvimconf.modules.lazygit")
-import("git-worktree", require("nvimconf.modules.worktree"))
+-- Lsp
+import("mason", require("nvimconf.modules.lsp.mason"))
+import("mason-lspconfig", require("nvimconf.modules.lsp.mason-lspconfig"))
+import("lspconfig", require("nvimconf.modules.lsp.lspconfig"))
+import("lspsaga", require("nvimconf.modules.lsp.lspsaga"))
 
--- Writing Stuff
--- import("nvim-treesitter", require("nvimconf.modules.treesitter"))
-import("nvim-autopairs", require("nvimconf.modules.autopairs"))
-import("Comment", require("nvimconf.modules.comment"))
+-- Syntax
+import("nvim-treesitter", require("nvimconf.modules.syntax.treesitter"))
+import("hlargs", require("nvimconf.modules.syntax.hlargs"))
+import("nvimconf.modules.syntax.markdown")
+
+-- Ui
+import("bufferline", require("nvimconf.modules.ui.bufferline"))
+import("indent_blankline", require("nvimconf.modules.ui.indent"))
+import("dressing", require("nvimconf.modules.ui.dressing"))
+import("telescope", require("nvimconf.modules.ui.telescope"))
+import("alpha", require("nvimconf.modules.ui.alpha"))
+import("project_nvim", require("nvimconf.modules.ui.project"))
+
+-- Writing
+import("nvim-autopairs", require("nvimconf.modules.writing.autopairs"))
+import("Comment", require("nvimconf.modules.writing.comment"))
+import("cmp", require("nvimconf.modules.writing.cmp"))
 
 -- Plugins
 import("nvimconf.modules.plugins")
-
--- Lsp Stuff
--- import("mason", require("nvimconf.modules.lsp").manson)
--- import("manson-lspconfig", require("nvimconf.modules.lsp").manson_lspconfig)
--- import("lspconfig", require("nvimconf.modules.lsp").lspconfig)
--- import("lspsaga", require("nvimconf.modules.lsp").lspsaga)
-
--- Cmp Stuff
--- import("cmp", require("lua.nvimconf.modules.cmp"))
-
