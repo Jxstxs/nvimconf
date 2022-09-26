6b3dd605ed4884dde341b6dc08d0af382f31d5fc
Jxstxs <juleb0202@gmail.com>
2022-09-26 10:57:20 +0200

    fix: removed height (not available)

--- lua/nvimconf/modules/filemanager/nvim-tree.lua
+++ lua/nvimconf/modules/filemanager/nvim-tree.lua
@@ -25,7 +25,7 @@ return function(nt)
 			adaptive_size = false,
 			centralize_selection = false,
 			width = 30,
-			height = 30,
+			-- height = 30,
 			hide_root_folder = false,
 			side = "left",
 			preserve_window_proportions = false,
