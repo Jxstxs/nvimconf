c7398ac628c38c65bd5af0ded40de7d50fb79a18
Jxstxs <juleb0202@gmail.com>
2022-10-30 01:59:20 +0200

    feat: added after folder

---
+++ after/queries/lua/highlights.scm
@@ -0,0 +1,15 @@
+;; extends 
+
+(("in" @keyword) (#set! conceal "i"))
+(("local" @keyword) (#set! conceal "l"))
+(("and" @keyword.function) (#set! conceal "&"))
+(("return" @keyword.function) (#set! conceal "R"))
+(("then" @conditional) (#set! conceal "t"))
+(("else" @conditional) (#set! conceal "e"))
+(("elseif" @conditional) (#set! conceal "e"))
+(("end" @keyword.function) (#set! conceal "E"))
+(("do" @repeat) (#set! conceal "d"))
+(("for" @repeat) (#set! conceal "F"))
+(("function" @keyword.function) (#set! conceal "f"))
+(("if" @conditional) (#set! conceal "?"))
+((function_call name: (identifier) @function.builtin (#eq? @function.builtin "require")) (#set! conceal "r"))
