63ba0047a6dbfd76f774df3a2a8adb2e4e0232ff
Jxstxs <juleb0202@gmail.com>
2023-01-26 07:54:46 +0100

    feat: added setup function and options

--- lua/nvimconf/modules/norg/init.lua
+++ lua/nvimconf/modules/norg/init.lua
@@ -4,11 +4,25 @@ local export = require("nvimconf.modules.norg.export")
 local insert = require("nvimconf.modules.norg.insert")
 local utils = require("nvimconf.modules.norg.utils")
 
+local opts = {}
+
+local setup = function(opt)
+    opts = vim.tbl_deep_extend("force", opts, opt)
+end
+
+setup({
+    export_dirs = {
+        ""
+    }
+})
+
 local M = {
     exp_file = export.single_file,
     exp_dir = export.directory,
     ins_tab = insert.table,
     toggle_tab = utils.toggle_table,
+
+    opts = opts,
 }
 
 return M
