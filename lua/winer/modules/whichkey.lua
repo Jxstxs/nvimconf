ef3b0b806742ee070365ee073c32e4a5d7895038
Jxstxs <juleb0202@gmail.com>
2023-02-11 23:16:05 +0100

    fix: some improvements to whichkey

--- lua/winer/modules/whichkey.lua
+++ lua/winer/modules/whichkey.lua
@@ -1,17 +1,19 @@
-
 return {
     "folke/which-key.nvim",
-    init = function()
+    config = function()
+
         vim.o.timeout = true
         vim.o.timeoutlen = 300
+
         local wk = require("which-key")
+
         wk.setup({
             plugins = {
                 marks = true, -- shows a list of your marks on ' and `
                 registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
                 spelling = {
-                    enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-                    suggestions = 20, -- how many suggestions should be shown in the list?
+                    enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+                    suggestions = 30, -- how many suggestions should be shown in the list?
                 },
                 -- the presets plugin, adds help for a bunch of default keybindings in Neovim
                 -- No actual key bindings are created
@@ -45,7 +47,7 @@ return {
                 scroll_up = '<c-u>', -- binding to scroll up inside the popup
             },
             window = {
-                border = "none", -- none, single, double, shadow
+                border = "single", -- none, single, double, shadow
                 position = "bottom", -- bottom, top
                 margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
                 padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
@@ -55,10 +57,10 @@ return {
                 height = { min = 4, max = 25 }, -- min and max height of the columns
                 width = { min = 20, max = 50 }, -- min and max width of the columns
                 spacing = 3, -- spacing between columns
-                align = "left", -- align columns left, center or right
+                align = "center", -- align columns left, center or right
             },
             ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-            hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
+            hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
             show_help = true, -- show help message on the command line when the popup is visible
             show_keys = true, -- show the currently pressed key and its label as a message in the command line
             triggers = "auto", -- automatically setup triggers
@@ -77,8 +79,17 @@ return {
                 filetypes = { "TelescopePrompt" },
             },
         })
-        local k = require("winer.keymaps").keys
-        wk.register(k.n.mappings, k.n.opts)
-        wk.register(k.v.mappings, k.v.opts)
+
+        -- Normal Mode
+        wk.register({
+            V = "[V]im Stuff",
+            f = "[F]inder Stuff",
+            g = "[G]it Stuff",
+        }, { mode = "n", prefix = "<leader>" })
+
+        -- Visual Mode
+        wk.register({
+            T = { name = "Tester"}
+        }, { mode = "v", prefix = "<leader>" })
     end,
 }
