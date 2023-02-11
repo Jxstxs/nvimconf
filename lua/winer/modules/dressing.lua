a80b2cd9836f4f5031d3d01102386c6b7fa303a6
Jxstxs <juleb0202@gmail.com>
2023-02-11 23:24:16 +0100

    fix: added lazyloading and fixed deprecation

--- lua/winer/modules/dressing.lua
+++ lua/winer/modules/dressing.lua
@@ -1,5 +1,6 @@
 return {
     "stevearc/dressing.nvim",
+    event = "UIEnter",
     config = function()
         require("dressing").setup({
             input = {
@@ -32,10 +33,12 @@ return {
                 max_width = { 140, 0.9 },
                 min_width = { 20, 0.2 },
 
-                -- Window transparency (0-100)
-                winblend = 10,
-                -- Change default highlight groups (see :help winhl)
-                winhighlight = "",
+                win_options = {
+                    -- Window transparency (0-100)
+                    winblend = 10,
+                    -- Change default highlight groups (see :help winhl)
+                    winhighlight = "",
+                },
 
                 -- Set to `false` to disable
                 mappings = {
@@ -120,10 +123,12 @@ return {
                     -- 'editor' and 'win' will default to being centered
                     relative = "editor",
 
-                    -- Window transparency (0-100)
-                    winblend = 10,
-                    -- Change default highlight groups (see :help winhl)
-                    winhighlight = "",
+                    win_options = {
+                        -- Window transparency (0-100)
+                        winblend = 10,
+                        -- Change default highlight groups (see :help winhl)
+                        winhighlight = "",
+                    },
 
                     -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
                     -- the min_ and max_ options can be a list of mixed types.
