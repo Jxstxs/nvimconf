3b1e4fab04085fc1ca79d4f1e2e0e7f04b2d2725
Jxstxs <juleb0202@gmail.com>
2023-02-11 23:10:21 +0100

    feat: added basic init

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
----@diagnostic disable-next-line: undefined-global
--- vim.api.nvim_set_current_dir("C:\\Users\\dave")
-
-lgT = nil
-
 require("winer.options")
-require("winer.keymaps").base()
-
+require("winer.keymaps")
 require("winer.lazy")
