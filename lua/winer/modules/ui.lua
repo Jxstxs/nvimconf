2a45fd439587b79d8ce00500a57a34a09b45e083
Jxstxs <juleb0202@gmail.com>
2023-02-12 01:35:28 +0100

    fix: moved indentline and colorscheme to ui

--- lua/winer/modules/ui.lua
+++ lua/winer/modules/ui.lua
@@ -25,5 +25,28 @@ return {
         event = "UIEnter",
         config = true
     },
+    {
+        "projekt0n/github-nvim-theme",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("github-theme").setup({ theme_style = "dark_default", })
+        end,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "BufRead",
+        config = function()
+            vim.opt.list = true
+            vim.opt.listchars:append("eol:↴")
+
+            require("indent_blankline").setup({
+                show_end_of_line = true,
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+            })
+        end
+    },
 
 }
