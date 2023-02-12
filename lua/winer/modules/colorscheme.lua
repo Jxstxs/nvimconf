3087f08c219816fec10629288da35e0213f54356
Jxstxs <juleb0202@gmail.com>
2023-02-12 01:01:03 +0100

    fix: moved each to ui

--- lua/winer/modules/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-
-return {
-    "projekt0n/github-nvim-theme",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("github-theme").setup({ theme_style = "dark_default", })
-    end,
-}
