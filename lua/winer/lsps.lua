f4af456a506efcd1770cc09ad13ba91f5525c593
Jxstxs <juleb0202@gmail.com>
2023-02-12 01:38:18 +0100

    fix: removed unused stuff

--- lua/winer/lsps.lua
+++ lua/winer/lsps.lua
@@ -3,15 +3,9 @@ return {
     "vimls", -- vim script
     "sqlls", -- sql
     "jedi_language_server", -- python
-    "intelephense", -- php
     "marksman", -- markdown
     "sumneko_lua", -- lua
-    "quick_lint_js", -- javascript
     "jsonls", -- json
-    "html", -- html
-    "cssls", -- css
     "clangd", -- c, c++
     "bashls", -- bash
-    -- "awk_ls", -- awk
-    -- "pasls"
 }
