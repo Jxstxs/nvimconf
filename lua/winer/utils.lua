b7d548dfe44e8fe56e8e043adbdc27181d1dba22
Jxstxs <juleb0202@gmail.com>
2023-02-12 19:43:12 +0100

    feat: added telescope finder for selections

--- lua/winer/utils.lua
+++ lua/winer/utils.lua
@@ -44,7 +44,48 @@ M.map = {
 M.pvi = function(tbl)
     print(vim.inspect(tbl))
 end
+
+--- Opens a Telescope Picker
+---@param _picker table the settings for the Picker
+---@diagnostic disable-next-line: 214
+M.get_selection = function(_picker)
+    local selector = require("telescope.pickers").new(_picker)
+    selector:find()
+end
+
+M.picker = {
+    --- Creates an Telescope Finder with a Table as Entries
+    ---@param values table the Entries to be displayed
+    ---@param returner fun(result: any): nil the function which handles the results
+    ---@return string[] entires the selected Entries
+    table = function(values, returner)
+        local finders = require("telescope.finders")
+        local sorters = require("telescope.sorters")
+        local actions = require("telescope.actions")
+        local state = require("telescope.actions.state")
+
+        return {
+            finder = finders.new_table(values),
+            sorter = sorters.get_generic_fuzzy_sorter({}),
+            attach_mappings = function(prompt_bufnr, map)
+                map("i", "<CR>", function()
+                    local selection = state.get_current_picker(prompt_bufnr):get_multi_selection()
+                    actions.close(prompt_bufnr)
+                    returner(selection)
+                end)
+
+                return true
+            end,
+        }
     end,
 }
+
+--- Warps a function in a Corutine
+---@param fnc function the function to run
+M.warpper = function(fnc)
+    coroutine.resume(coroutine.create(function()
+        fnc()
+    end))
+end
 
 return M
