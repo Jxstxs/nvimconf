f9b60e3479dadb3cb554ab98906cc99e33a4246d
Jxstxs <juleb0202@gmail.com>
2023-02-12 01:18:31 +0100

    fix: clean up hopefully

--- lua/winer/modules/nvim-tree.lua
+++ lua/winer/modules/nvim-tree.lua
@@ -10,35 +10,10 @@ return {
 
         require("nvim-tree").setup({
             auto_reload_on_write = true,
-            create_in_closed_folder = false,
             disable_netrw = true,
-            hijack_cursor = false,
             hijack_netrw = true,
-            hijack_unnamed_buffer_when_opening = false,
-            ignore_buffer_on_setup = false,
-            open_on_setup = false,
-            open_on_setup_file = false,
-            open_on_tab = false,
-            sort_by = "name",
-            -- root_dirs = {},
-            -- prefer_startup_root = false,
-            -- sync_root_with_cwd = true,
-            reload_on_bufenter = false,
-            respect_buf_cwd = false,
-            -- on_attach = "disable", -- function(bufnr). If nil, will use the deprecated mapping strategy
-            -- remove_keymaps = false, -- boolean (disable totally or not) or list of key (lhs)
             view = {
-                adaptive_size = false,
-                centralize_selection = false,
-                width = 30,
-                -- height = 30,
-                hide_root_folder = false,
                 side = "left",
-                preserve_window_proportions = false,
-                number = false,
-                relativenumber = false,
-                signcolumn = "yes",
-                -- @deprecated
                 mappings = {
                     custom_only = false,
                     list = {
@@ -55,159 +30,25 @@ return {
                     },
                 },
             },
-            renderer = {
-                add_trailing = false,
-                group_empty = false,
-                highlight_git = false,
-                full_name = false,
-                highlight_opened_files = "none",
-                root_folder_modifier = ":~",
-                -- indent_width = 2,
-                indent_markers = {
-                    enable = false,
-                    inline_arrows = true,
-                    icons = {
-                        corner = "└",
-                        edge = "│",
-                        item = "│",
-                        -- bottom = "─",
-                        none = " ",
-                    },
-                },
-                icons = {
-                    webdev_colors = true,
-                    git_placement = "before",
-                    padding = " ",
-                    symlink_arrow = " ➛ ",
-                    show = {
-                        file = true,
-                        folder = true,
-                        folder_arrow = true,
-                        git = true,
-                    },
-                    glyphs = {
-                        default = "",
-                        symlink = "",
-                        bookmark = "",
-                        folder = {
-                            arrow_closed = "",
-                            arrow_open = "",
-                            default = "",
-                            open = "",
-                            empty = "",
-                            empty_open = "",
-                            symlink = "",
-                            symlink_open = "",
-                        },
-                        git = {
-                            unstaged = "",
-                            staged = "S",
-                            unmerged = "",
-                            renamed = "➜",
-                            untracked = "U",
-                            deleted = "",
-                            ignored = "◌",
-                        },
-                    },
-                },
-                special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
-            },
-            hijack_directories = {
+            sync_root_with_cwd = true,
+            view = { float = {
                 enable = true,
-                auto_open = true,
-            },
+                quit_on_focus_loss = false,
+            } },
+            renderer = { special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" }, },
             update_focused_file = {
-                enable = false,
+                enable = true,
                 update_root = true,
-                ignore_list = {},
-            },
-            ignore_ft_on_setup = {},
-            system_open = {
-                cmd = "",
-                args = {},
-            },
-            diagnostics = {
-                enable = true,
-                show_on_dirs = true,
-                icons = {
-                    hint = "",
-                    info = "",
-                    warning = "",
-                    error = "",
-                },
-            },
-            filters = {
-                dotfiles = false,
-                custom = {},
-                exclude = {},
-            },
-            filesystem_watchers = {
-                enable = true,
-                -- debounce_delay = 50,
-            },
-            git = {
-                enable = true,
-                ignore = true,
-                -- show_on_dirs = true,
-                timeout = 400,
             },
             actions = {
-                use_system_clipboard = true,
-                change_dir = {
-                    enable = true,
-                    global = false,
-                    restrict_above_cwd = false,
-                },
-                expand_all = {
-                    max_folder_discovery = 300,
-                    -- exclude = {},
-                },
-                -- file_popup = {
-                -- open_win_config = {
-                -- col = 1,
-                -- row = 1,
-                -- relative = "cursor",
-                -- border = "shadow",
-                -- style = "minimal",
-                -- },
-                -- },
-                open_file = {
-                    quit_on_open = false,
-                    resize_window = true,
-                    window_picker = {
-                        enable = true,
-                        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-                        -- exclude = {
-                        -- filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-                        -- buftype = { "nofile", "terminal", "help" },
-                        -- },
-                    },
-                },
-                remove_file = {
-                    close_window = true,
-                },
-            },
-            trash = {
-                cmd = "gio trash",
-                require_confirm = true,
-            },
-            live_filter = {
-                prefix = "[FILTER]: ",
-                always_show_folders = true,
-            },
-            log = {
-                enable = false,
-                truncate = false,
-                types = {
-                    all = false,
-                    config = false,
-                    copy_paste = false,
-                    -- dev = false,
-                    diagnostics = false,
-                    git = false,
-                    profile = false,
-                    watcher = false,
-                },
+                file_popup = { open_win_config = {
+                    relative = "cursor",
+                    border = "single",
+                } },
+                open_file = { window_picker = { exclude = {
+                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                    buftype = { "nofile", "terminal", "help" },
+                } } },
             },
         })
     end
