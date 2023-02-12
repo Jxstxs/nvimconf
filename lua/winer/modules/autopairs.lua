f2570a51aa1a39410884aeab892c5dc0b2c2a52c
Jxstxs <juleb0202@gmail.com>
2023-02-12 01:00:44 +0100

    fix: autopairs moved to core

--- lua/winer/modules/autopairs.lua
+++
@@ -1,8 +0,0 @@
-
-return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-        require("nvim-autopairs").setup()
-    end
-}
---
+++ lua/winer/modules/core.lua
@@ -0,0 +1,14 @@
+local m = require("winer.utils").map
+
+return {
+    { "famiu/bufdelete.nvim", keys = {
+        { m.ld("q"), m.cmd("Bdelete"), desc = "Close Buffer" }
+    } },
+{
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+        require("nvim-autopairs").setup({ disable_in_macro = true, })
+    end
+}
+}
