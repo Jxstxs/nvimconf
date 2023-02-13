422cab77e76a76133d8e3213d9e0d21d86d2b283
Jxstxs <juleb0202@gmail.com>
2023-02-14 00:32:52 +0100

    feat: added highlighted yank

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 require("winer.options")
 require("winer.keymaps")
 require("winer.lazy")
+
+-- Highlight Yanked Text
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+    callback = function()
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+    end,
+})
