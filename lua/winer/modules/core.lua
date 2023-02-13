df8f8ff7ec5ffaf1638792ca314e7fb12a2a6fc7
Jxstxs <juleb0202@gmail.com>
2023-02-13 01:59:24 +0100

    feat: added neorg

--- lua/winer/modules/core.lua
+++ lua/winer/modules/core.lua
@@ -86,4 +86,38 @@ return {
             })
         end,
     },
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        event = { "BufRead", "BufNewFile" },
+        opts = {
+            load = {
+                ["core.defaults"] = {},
+                ["core.norg.esupports.metagen"] = {
+                    config = {
+                        type = "auto",
+                    },
+                },
+                -- ["core.presenter"] = { config = { zen_mode = "truezen" } },
+                ["core.export"] = {},
+                ["core.norg.concealer"] = {},
+                ["core.norg.completion"] = { config = { engine = "nvim-cmp" } },
+                ["core.integrations.telescope"] = {}, -- FIX: Need Config?
+                ["core.norg.dirman"] = {
+                    config = {
+                        workspaces = {
+                            notes = "~/repos/personal/notes/",
+                            scholr = "~/repos/personal/scholr/",
+                        },
+                        index = "index.norg", -- The name of the main (root) .norg file
+                    },
+                },
+                ["core.norg.manoeuvre"] = { config = {} },
+            },
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-neorg/neorg-telescope",
+        },
+    },
 }
