205201f4e1ce9f4bf2a4bccc2d33c509dfcf575b
Jxstxs <juleb0202@gmail.com>
2023-02-13 01:53:57 +0100

    feat: added chatgpt

--- lua/winer/modules/ai.lua
+++ lua/winer/modules/ai.lua
@@ -1,4 +1,30 @@
+local m = require("winer.utils").map
+
 return {
     { "jcdickinson/codeium.nvim", event = "InsertEnter", config = true },
-    -- { "" }
+    {
+        "jackMort/ChatGPT.nvim",
+        opts = {
+            yank_register = "+",
+            settings_window = {
+                border = {
+                    style = "single",
+                },
+            },
+            chat_window = {
+                border = {
+                    style = "single",
+                },
+            },
+            chat_input = {
+                border = {
+                    style = "single",
+                },
+            },
+        },
+        config = true,
+        keys = {
+            { m.ld("Cc"), m.cmd("ChatGPT"), desc = "[C]hatGPT" },
+        },
+    },
 }
