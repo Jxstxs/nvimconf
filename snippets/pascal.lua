f5d9749b3f4997c6a72d0340bde5e40f47d6bb3f
Jxstxs <juleb0202@gmail.com>
2023-02-16 00:18:14 +0100

    feat: added some usefull pascal snippets

---
+++ snippets/pascal.lua
@@ -0,0 +1,24 @@
+return {
+    s("fnc", fmt([[
+    Function {}({}): {};
+    {}
+        {}
+        Result := {};
+    end
+    ]], {
+        i(1, "fnc"),
+        i(2, "args"),
+        i(3, "type"),
+        c(4, {
+            sn(1, fmt([[
+            var
+                {}
+            Begin
+            ]], {i(1)})),
+            t("Begin")
+        }),
+        i(5), i(6)
+    })),
+    s("vdec", fmt("{}: {}{}", {i(1,"var"),i(2,"type"), c(3, {t(";"),t("")})})),
+    s("vset", fmt("{} := {};", {i(1,"var"),i(2,"data")})),
+}
