5d7d7c3c45ec43b9500ab24d0fc42f2bb75c9e15
Jxstxs <juleb0202@gmail.com>
2023-03-30 23:43:45 +0200

    feat: added indent lines

--- lua/dafne/plugins/ui.lua
+++ lua/dafne/plugins/ui.lua
@@ -113,6 +113,18 @@ return {
     },
 
     {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "BufRead",
+        config = function()
+            require("indent_blankline").setup({
+                show_end_of_line = true,
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+            })
+        end,
+    },
+    {
         "glepnir/dashboard-nvim",
         config = function()
             require("dashboard").setup({
