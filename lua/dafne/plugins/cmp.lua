576bfdc45d8b3da3592efc0571241b44be072b25
Jxstxs <juleb0202@gmail.com>
2023-03-31 00:54:06 +0200

    fix: format

--- lua/dafne/plugins/cmp.lua
+++ lua/dafne/plugins/cmp.lua
@@ -16,8 +16,7 @@ return {
             ---@diagnostic disable-next-line: deprecated
             local unpack = unpack or table.unpack
             local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0
-                and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
         end
 
         local cmp = require("cmp")
@@ -25,7 +24,7 @@ return {
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
+                    require("luasnip").lsp_expand(args.body)
                 end,
             },
             window = {
