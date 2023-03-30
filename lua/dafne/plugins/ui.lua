5034259f64ed1e2aa50c7d1c0abe14439f5206ae
Jxstxs <juleb0202@gmail.com>
2023-03-30 23:43:26 +0200

    feat: added dashboard

--- lua/dafne/plugins/ui.lua
+++ lua/dafne/plugins/ui.lua
@@ -1,5 +1,22 @@
 local m = require("dafne.util").map
 
+--- Returns a merged table from opts and defaults
+---@param opts table?
+---@return table
+local section = function(opts)
+    local defaults = {
+        icon = " ",
+        icon_hl = "Title",
+        desc = "",
+        desc_hl = "String",
+        key = "",
+        keymap = "",
+        key_hl = "Number",
+        action = "",
+    }
+    return vim.tbl_deep_extend("force", defaults, opts)
+end
+
 return {
     {
         "NTBBloodbath/doom-one.nvim",
@@ -93,5 +110,58 @@ return {
         opts = {
             numhl = true,
         },
+    },
+
+    {
+        "glepnir/dashboard-nvim",
+        config = function()
+            require("dashboard").setup({
+                theme = "doom",
+                config = {
+                    header = {
+                        "██████╗░░█████╗░███████╗███╗░░██╗███████╗",
+                        "██╔══██╗██╔══██╗██╔════╝████╗░██║██╔════╝",
+                        "██║░░██║███████║█████╗░░██╔██╗██║█████╗░░",
+                        "██║░░██║██╔══██║██╔══╝░░██║╚████║██╔══╝░░",
+                        "██████╔╝██║░░██║██║░░░░░██║░╚███║███████╗",
+                        "╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚══╝╚══════╝",
+                    },
+                    center = {
+                        section({
+                            desc = "Find Files",
+                            key = "f",
+                            keymap = "SPC f f",
+                            action = "lua require('telescope.builtin').find_files()",
+                        }),
+                        section({
+                            desc = "Recent Files",
+                            key = "r",
+                            keymap = "SPC f o",
+                            action = "lua require('telescope.builtin').oldfiles()",
+                        }),
+                        section({
+                            desc = "Edit Config",
+                            key = "c",
+                            keymap = "SPC V c",
+                            action = "e $MYVIMRC",
+                        }),
+                        section({
+                            desc = "Open Lazy",
+                            key = "l",
+                            keymap = "SPC L",
+                            action = "Lazy",
+                        }),
+                        section({
+                            desc = "Quit",
+                            key = "q",
+                            keymap = "Z Z",
+                            action = "qa!",
+                        }),
+                    },
+                    footer = { "Dave Raves All Day Long" },
+                },
+            })
+        end,
+        dependencies = { { "nvim-tree/nvim-web-devicons" } },
     },
 }
