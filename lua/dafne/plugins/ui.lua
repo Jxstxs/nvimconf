ab73e7580209d5f1f4e41501d188fc077c5cc74e
Jxstxs <juleb0202@gmail.com>
2023-03-30 21:31:47 +0200

    fix: changed colorscheme

--- lua/dafne/plugins/ui.lua
+++ lua/dafne/plugins/ui.lua
@@ -1,9 +1,28 @@
 return {
     {
-        "nyoom-engineering/oxocarbon.nvim",
+        "NTBBloodbath/doom-one.nvim",
         config = function()
-            vim.opt.background = "dark"
-            vim.cmd("colorscheme oxocarbon")
+            vim.opt.termguicolors = true
+            vim.g.doom_one_cursor_coloring = false
+            vim.g.doom_one_terminal_colors = true
+            vim.g.doom_one_italic_comments = true
+            vim.g.doom_one_enable_treesitter = true
+            vim.g.doom_one_diagnostics_text_color = false
+            vim.g.doom_one_transparent_background = false
+
+            vim.g.doom_one_plugin_neorg = true
+            vim.g.doom_one_plugin_barbar = false
+            vim.g.doom_one_plugin_telescope = true
+            vim.g.doom_one_plugin_neogit = false
+            vim.g.doom_one_plugin_nvim_tree = true
+            vim.g.doom_one_plugin_dashboard = true
+            vim.g.doom_one_plugin_startify = false
+            vim.g.doom_one_plugin_whichkey = true
+            vim.g.doom_one_plugin_indent_blankline = true
+            vim.g.doom_one_plugin_vim_illuminate = false
+            vim.g.doom_one_plugin_lspsaga = true
+
+            vim.cmd("colorscheme doom-one")
         end,
     },
 
