802143e72af03d70b8876597bab8482327f68b4d
Jxstxs <juleb0202@gmail.com>
2023-05-05 20:19:05 +0200

    feat(ts): added playground

--- lua/dafne/plugins/syntax.lua
+++ lua/dafne/plugins/syntax.lua
@@ -4,15 +4,23 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        dependencies = { "nvim-treesitter/playground" },
         config = function()
             require("nvim-treesitter.configs").setup({
                 ensure_installed = require("dafne.installs").ts,
                 sync_install = false,
                 auto_install = true,
-
                 highlight = {
                     enable = true,
                     additional_vim_regex_highlighting = false,
+                },
+                playground = {
+                    enable = true,
+                },
+                query_linter = {
+                    enable = true,
+                    use_virtual_text = true,
+                    lint_events = { "BufWrite", "CursorHold" },
                 },
             })
         end,
