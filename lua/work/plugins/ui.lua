76bc6f284bc78baba948a54fce4d0cc72100d981
Julius Beier - CTI GmbH <julius.beier@cti-lean.com>
2023-11-15 08:26:51 +0100

    fix: disabled italic strings

--- lua/work/plugins/ui.lua
+++ lua/work/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
                 options = {
                     styles = {
                         comments = "italic",
-                        strings = "italic",
+                        -- strings = "italic",
                     },
                 },
             })
