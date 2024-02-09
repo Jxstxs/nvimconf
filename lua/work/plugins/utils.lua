17bf711f3df08a1df458ac784fa8cc554ec8fc48
Julius Beier - CTI GmbH <julius.beier@cti-lean.com>
2024-02-09 22:36:09 +0100

    fix: removed luasnip jsregexp dep

--- lua/work/plugins/utils.lua
+++ lua/work/plugins/utils.lua
@@ -83,7 +83,6 @@ return {
     {
         "L3MON4D3/LuaSnip",
         event = "InsertEnter",
-        build = "make install_jsregexp",
         config = function()
             local ls = require("luasnip")
             ls.setup({
