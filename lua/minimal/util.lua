12c4edefd43bb609ec6f0c8299ed482354e13343
Jxstxs <juleb0202@gmail.com>
2024-03-01 22:41:12 +0100

    feat: added util.merge_tbl()

--- lua/minimal/util.lua
+++ lua/minimal/util.lua
@@ -9,21 +9,21 @@ M.map = {
     ---@param rhs string|function the thing to execute when binding is activated
     ---@param opts table? the options for vim.keymap.set
     n = function(lhs, rhs, opts)
-        ks("n", lhs, rhs, vim.tbl_deep_extend("force", dopts, opts or {}))
+        ks("n", lhs, rhs, M.merge_tbl(dopts, opts))
     end,
     --- Maps to Normal Mode
     ---@param lhs string the keys for the binding
     ---@param rhs string|function the thing to execute when binding is activated
     ---@param opts table? the options for vim.keymap.set
     i = function(lhs, rhs, opts)
-        ks("i", lhs, rhs, vim.tbl_deep_extend("force", dopts, opts or {}))
+        ks("i", lhs, rhs, M.merge_tbl(dopts, opts))
     end,
     --- Maps to Normal Mode
     ---@param lhs string the keys for the binding
     ---@param rhs string|function the thing to execute when binding is activated
     ---@param opts table? the options for vim.keymap.set
     v = function(lhs, rhs, opts)
-        ks("v", lhs, rhs, vim.tbl_deep_extend("force", dopts, opts or {}))
+        ks("v", lhs, rhs, M.merge_tbl(dopts, opts))
     end,
     --- Returns a String as Vim Command
     ---@param _cmd string the Command to warp
@@ -49,6 +49,14 @@ M.map = {
 ---@param tbl table the table to print
 M.pvi = function(tbl)
     print(vim.inspect(tbl))
+end
+
+--- Returns a merged table which favors t1
+---@param t1 table? base table
+---@param t2 table? expanding table
+---@return table table the merged table
+M.merge_tbl = function(t1, t2)
+    return vim.tbl_deep_extend("force", t1 or {}, t2 or {})
 end
 
 return M
