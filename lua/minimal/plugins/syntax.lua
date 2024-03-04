5d385d9021bffd16059a57d6feb19e8132aa8c28
Jxstxs <juleb0202@gmail.com>
2024-03-04 14:03:37 +0100

    feat: added indent and inc selection modules treesitter

--- lua/minimal/plugins/syntax.lua
+++ lua/minimal/plugins/syntax.lua
@@ -19,6 +19,18 @@ return {
                     use_virtual_text = true,
                     lint_events = { "BufWrite", "CursorHold" },
                 },
+                indent = {
+                    enable = true
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
+                },
             })
         end,
     },
