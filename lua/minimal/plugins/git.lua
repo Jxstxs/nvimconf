89e58d47ac3143a2be304730480433a7b1860679
Jxstxs <juleb0202@gmail.com>
2024-03-15 23:31:10 +0100

    chore: moved gitsigns to own file

---
+++ lua/minimal/plugins/git.lua
@@ -0,0 +1,19 @@
+local m = require("minimal.util").map
+
+return {
+
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "BufReadPre",
+        keys = {
+            { m.ld("gp"), m.cmd("Gitsigns preview_hunk"),              desc = "[P]review Hunk" },
+            { m.ld("gb"), m.cmd("Gitsigns toggle_current_line_blame"), desc = "Line [B]lame" },
+            { m.ld("gr"), m.cmd("Gitsigns reset_hunk"),                desc = "[R]eset Hunk" },
+            { m.ld("gl"), m.cmd("Gitsigns toggle_linehl"),             desc = "[L]ine Highlight" },
+            { m.ld("gd"), m.cmd("Gitsigns toggle_deleted"),            desc = "Show [D]eleted" },
+        },
+        opts = {
+            numhl = true,
+        },
+    },
+}
