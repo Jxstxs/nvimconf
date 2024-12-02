ea34de813e866820a7fc387a262a3a2116fde5ce
Jxstxs <juleb0202@gmail.com>
2024-12-02 08:29:43 +0100

    fix: treesitter wont get setup properly

--- lua/minimal/plugins/syntax.lua
+++ lua/minimal/plugins/syntax.lua
@@ -7,66 +7,68 @@ return {
             "nvim-treesitter/nvim-treesitter-textobjects"
         },
         build = ":TSUpdate",
-        opts = {
-            ensure_installed = require("minimal.installs").ts,
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = true
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "gnn",
-                    node_incremental = "grn",
-                    scope_incremental = "grc",
-                    node_decremental = "grm",
-                },
-            },
-            textobjects = {
-                enable = true,
-                lsp_interop = {
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = require("minimal.installs").ts,
+                sync_install = false,
+                auto_install = true,
+                highlight = {
                     enable = true,
-                    border = "rounded",
-                    peek_definition_code = {
-                        [m.ld("pf")] = "@function.outer",
-                        [m.ld("pc")] = "@class.outer",
-                        [m.ld("pp")] = "@parameter.outer",
-                        [m.ld("pa")] = "@assignment.outer",
-                    },
+                    additional_vim_regex_highlighting = false,
                 },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        [m.ld("sp")] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        [m.ld("sP")] = "@parameter.inner",
-                    },
+                indent = {
+                    enable = true
                 },
-                select = {
+                incremental_selection = {
                     enable = true,
-                    lookahead = true,
-                    lookbehind = true,
                     keymaps = {
-                        ab = "@block.outer",
-                        ib = "@block.inner",
-                        ac = "@class.outer",
-                        ic = "@class.inner",
-                        aC = "@conditional.outer",
-                        iC = "@conditional.inner",
-                        af = "@function.outer",
-                        ["if"] = "@function.inner",
-                        al = "@loop.outer",
-                        il = "@loop.inner",
-                    }
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
                 },
-            }
-        },
+                textobjects = {
+                    enable = true,
+                    lsp_interop = {
+                        enable = true,
+                        border = "rounded",
+                        peek_definition_code = {
+                            [m.ld("pf")] = "@function.outer",
+                            [m.ld("pc")] = "@class.outer",
+                            [m.ld("pp")] = "@parameter.outer",
+                            [m.ld("pa")] = "@assignment.outer",
+                        },
+                    },
+                    swap = {
+                        enable = true,
+                        swap_next = {
+                            [m.ld("sp")] = "@parameter.inner",
+                        },
+                        swap_previous = {
+                            [m.ld("sP")] = "@parameter.inner",
+                        },
+                    },
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        lookbehind = true,
+                        keymaps = {
+                            ab = "@block.outer",
+                            ib = "@block.inner",
+                            ac = "@class.outer",
+                            ic = "@class.inner",
+                            aC = "@conditional.outer",
+                            iC = "@conditional.inner",
+                            af = "@function.outer",
+                            ["if"] = "@function.inner",
+                            al = "@loop.outer",
+                            il = "@loop.inner",
+                        }
+                    },
+                }
+            })
+        end,
     },
 
     {
